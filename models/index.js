0fa0a169c0c4d89d8ba49f0ce4ac12d61c7508e2
Phoenix Staley <PhoenixStaley121@hotmail.com>
2022-05-18 11:32:58 -0700

    Add models and connect them

--- models/index.js
+++ models/index.js
@@ -5,12 +5,34 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsToOne(Category, {
+  foreignKey: 'category_id'
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onUpdate: 'CASCADE',
+  onDelete: 'SET NULL'
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: {
+    ProductTag,
+    unique: false
+  },
+  as: 'product_tags'
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    ProductTag,
+    unique: false
+  },
+  as: 'tagged_products'
+});
 
 module.exports = {
   Product,
