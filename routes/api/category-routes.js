79d128dfd2dfd9efac8dc987893bba1dcf9536ae
Phoenix Staley <PhoenixStaley121@hotmail.com>
2022-05-18 15:27:17 -0700

    Implement /api/category routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,26 +3,97 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  try {
+    categoryData = await Category.findAll();
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    const categoryData = await Category.findByPk(req.params.id, {
+      // Is through: Product the right option? or should it be through another model?
+      include: [{ model: Product }]
+    });
+
+    if (!categoryData) {
+      res.status(404).json({ message: 'There is no category matching this id!' });
+      return;
+    }
+
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  try {
+    if (!req.body.category_name) {
+      res.status(400).json({ message: 'Request body must have a category_name property.' });
+      return;
+    }
+
+    const categoryData = await Category.create(req.body);
+    res.status(201).json(categoryData);
+
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  try {
+    const chosenCategory = await Category.findByPk(req.params.id);
+
+    if (!chosenCategory) {
+      res.status(404).json({ message: 'There is no category matching this id!' });
+      return;
+    }
+    if (!req.body.category_name) {
+      res.status(400).json({ message: 'PUT requests must contain a valid body.' });
+      return;
+    }
+    
+    const categoryData = await Category.update(req.body, {
+      where: {
+        id: req.params.id
+      }
+    });
+    res.status(200).json({ message: `Category with ID #${req.params.id} was updated.`, update: req.body});
+
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  try {
+    const doesExist = await Category.findByPk(req.params.id);
+    if (!doesExist) {
+      res.status(404).json({ message: 'There is no category matching this id!' });
+    }
+
+    await Category.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    res.status(200).json({ message: `Category with ID #${req.params.id} has been deleted.` });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
